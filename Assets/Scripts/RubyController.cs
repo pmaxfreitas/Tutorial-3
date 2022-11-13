19bc221797589bca6875825d658a20097a5a0ffc
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 12:58:33 +0000

    Nov 13, 2022, 7:58 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -11,7 +11,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = 
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
