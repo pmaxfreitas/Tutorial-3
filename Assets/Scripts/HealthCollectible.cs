08dd72215df1628f4b999f86dc022482a12675a2
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 14:45:47 +0000

    Nov 13, 2022, 9:45 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
 
     }
