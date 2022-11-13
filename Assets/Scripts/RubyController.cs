dad6df487cfce49241e4e85da75feef5b38e979d
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 00:46:00 +0000

    Nov 12, 2022, 7:46 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,6 +13,7 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
     }
 }
