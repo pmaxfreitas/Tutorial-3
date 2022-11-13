8a3f830c843597091951d66ff1b891c560ab1995
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 15:02:53 +0000

    Nov 13, 2022, 10:02 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -10,7 +10,7 @@ public class HealthCollectible : MonoBehaviour
 
         if (controller != null)
         {
-            if (controller.currentHealth < controller.maxHealth)
+            if (controller.health < controller.maxHealth)
             {
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
