f5f61838f0bb53b1125a98fce7940e03fec0fe00
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 15:58:19 +0000

    Nov 13, 2022, 10:58 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed = 3.0f;
+    public bool vertical;
 
     Rigidbody2D rigidbody2D;
 
@@ -23,6 +24,11 @@ public class EnemyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + speed - 
+        }
         position.x = position.x + speed * Time.deltaTime;
 
         rigidbody2D.MovePosition(position);
