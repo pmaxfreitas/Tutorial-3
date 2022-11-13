737282f8682423fe80be04cad55ef728d8c3e536
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 09:58:30 +0000

    Nov 13, 2022, 4:58 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,7 +14,6 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
         transform.position = position;
