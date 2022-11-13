aa1cc784bf63cc66b0175066e708c16b1e8e51f9
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 16:01:40 +0000

    Nov 13, 2022, 11:01 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -27,9 +27,12 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = position.y + speed * Time.deltaTime;
+            position.y = position.y + Time.deltaTime * speed;
         }
-        position.x = position.x + speed * Time.deltaTime;
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
 
         rigidbody2D.MovePosition(position);
     }
