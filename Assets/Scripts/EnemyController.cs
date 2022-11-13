0858f8495e2c2c6a97d5aa863e2164576854218c
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-13 15:56:34 +0000

    Nov 13, 2022, 10:56 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,7 +11,7 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
