874081539d7c68a7714463d1b1e2f92dab7264b2
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-14 01:46:18 +0000

    Nov 13, 2022, 8:46 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -91,7 +91,8 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
     void Launch()
