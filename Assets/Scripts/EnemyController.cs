76037a97083971de4c7a6e48d477f063dd9a3d26
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-14 01:11:14 +0000

    Nov 13, 2022, 8:11 PM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -86,5 +86,7 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2D.simulated = false;
         animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
