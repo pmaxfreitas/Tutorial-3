53f29cd757b3e998856294a4520c3fd3c3dd18af
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-14 02:00:23 +0000

    Nov 13, 2022, 9:00 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -69,7 +69,11 @@ public class RubyController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.X))
         {
-            
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2D.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+            }
         }
     }
 
