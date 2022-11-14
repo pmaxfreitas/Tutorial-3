22c7008ce3d2a3838dcba0813b49f7411428feb7
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-14 00:10:23 +0000

    Nov 13, 2022, 7:10 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -23,6 +23,8 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
