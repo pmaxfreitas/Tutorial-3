cdc096e54b270b83f4efbff57e6295038bf08489
pmax16 <55547979+pmax16@users.noreply.github.com>
2022-11-14 00:12:37 +0000

    Nov 13, 2022, 7:12 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -88,4 +88,9 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+    }
 }
