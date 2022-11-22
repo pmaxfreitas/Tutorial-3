dbbb582c445363d15f79ca22fed870472b86fafb
pmaxfreitas <pmaxfreitas@hotmail.com>
2022-11-22 05:17:51 +0000

    Nov 22, 2022, 12:17 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -100,7 +100,7 @@ public class RubyController : MonoBehaviour
         {
             gameOver.text = "You Lose!\n\nPress R to Restart";
             gameOverBool = true;
-            rigidbody2D.enabled = false;
+            Destroy(rigidbody2D);
         }
 
         if(scoreValue >= 1)
