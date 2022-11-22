51f289d2a0099eb22cae1aebe79ca9362c8efb4c
pmaxfreitas <pmaxfreitas@hotmail.com>
2022-11-22 05:59:17 +0000

    Nov 22, 2022, 12:59 AM

--- Assets/Scripts/BkgMusic.cs
+++ Assets/Scripts/BkgMusic.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class BkgMusic : MonoBehaviour
 {
+    AudioSource audioSource;
+    AudioClip victory;
+    AudioClip loss;
+
     void Start()
     {
+        audioSource = gameObject.GetComponent<AudioSource>();
 
+        RubyController player = GameObject.Find("Ruby").GetComponent<RubyController>();
     }
 
     void Update()
     {
-        
+        if(player.currentHealth <= 0)
+        {
+            audioSource.clip = loss;
+        }
     }
 }
