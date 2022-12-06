42d971591e33168d7720de2ecc130164666878d4
pmaxfreitas <pmaxfreitas@hotmail.com>
2022-12-06 15:24:12 +0000

    Dec 6, 2022, 10:24 AM

--- Assets/Scripts/VendorController.cs
+++ Assets/Scripts/VendorController.cs
@@ -42,7 +42,7 @@ public class VendorController : MonoBehaviour
     public void SellItem()
     {
         RubyController player = GameObject.Find("Ruby").GetComponent<RubyController>();
-        if (itemOne)
+        if (GameObject == ItemOne)
         {
         player.cogsValue += 10;
         player.cogs.text = "Cogs: " + player.cogsValue.ToString();
