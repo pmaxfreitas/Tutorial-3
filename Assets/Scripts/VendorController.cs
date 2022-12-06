e15b15b2e17c99ea99af2552be8f6a73bda45a42
pmaxfreitas <pmaxfreitas@hotmail.com>
2022-12-06 18:54:58 +0000

    Dec 6, 2022, 1:54 PM

--- Assets/Scripts/VendorController.cs
+++ Assets/Scripts/VendorController.cs
@@ -14,8 +14,6 @@ public class VendorController : MonoBehaviour
     public GameObject itemOne;
     public GameObject itemTwo;
 
-    public EventSystem eventSystem;
-
     // Start is called before the first frame update
     void Start()
     {
