89964b80363c030859ccb4a91faacca9496f8d67
pmaxfreitas <pmaxfreitas@hotmail.com>
2022-12-06 14:54:55 +0000

    Dec 6, 2022, 9:54 AM

--- Assets/Scripts/VendorController.cs
+++ Assets/Scripts/VendorController.cs
@@ -9,6 +9,8 @@ public class VendorController : MonoBehaviour
 
     float timerDisplay;
 
+    public Button itemOne;
+
     // Start is called before the first frame update
     void Start()
     {
